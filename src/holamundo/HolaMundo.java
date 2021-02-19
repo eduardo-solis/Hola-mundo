8ffef043dc6ea8e2e102b4498017ce3aafe32552
Eduardo Rubén Solís Hernández <solishernandezeduardoruben@gmail.com>
2021-02-19 12:45:35 -0600

    Esta es mi primer version

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,18 @@
+
+package holamundo;
+
+/**
+ *
+ * @author eduardo-solis
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo soy eduardo-solis :D");
+    }
+    
+}
